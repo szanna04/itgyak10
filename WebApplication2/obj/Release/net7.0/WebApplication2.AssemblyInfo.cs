bdbd4760b13b7e3452f3044acc99cf8732ba88a6
szanna04 <anna.szerdahelyi04@gmail.com>
2024-12-02 21:22:42 +0100

    fel

--- WebApplication2/obj/Release/net7.0/WebApplication2.AssemblyInfo.cs
+++ WebApplication2/obj/Release/net7.0/WebApplication2.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebApplication2")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ac688079eef73caa595ca487f6a2213be303d59a")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebApplication2")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebApplication2")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
